cd8619b9a24ffd42efa40e17140382201eab4ba8
Niels Voss <code@nielsvoss.com>
2026-05-05 01:39:39 -0700

    Start working on defining std singular value decompositions

--- Mathlib/Analysis/InnerProductSpace/SingularVectors.lean
+++ Mathlib/Analysis/InnerProductSpace/SingularVectors.lean
@@ -75,14 +75,32 @@ section inner_product
 
 open InnerProductSpace
 
-variable (𝕜 : Type*) [RCLike 𝕜]
+variable {𝕜 : Type*} [RCLike 𝕜]
   {V : Type*} [NormedAddCommGroup V] [InnerProductSpace 𝕜 V] [FiniteDimensional 𝕜 V]
   {U : Type*} [NormedAddCommGroup U] [InnerProductSpace 𝕜 U] [FiniteDimensional 𝕜 U]
 
 def stdAdaptedOthonormalBasis {F : Set (Submodule 𝕜 V)} (hF : IsChain (· ≤ ·) F)
   : Fin (Module.finrank 𝕜 V) → V := sorry
 
-structure IsSingularValueDecomposition (T : V →ₗ[𝕜] U) (v : ℕ →₀ V) (u : ℕ →₀ U) : Prop where
+/--
+Note that the lists of singular vectors are allowed to continue on forever.
+-/
+structure IsSingularValueDecomposition (T : V →ₗ[𝕜] U) (u : ℕ → U) (v : ℕ → V) : Prop where
+  -- This sum is always finite since T.singularValues eventually terminates
   decomposition : T = ∑ᶠ i, (T.singularValues i : 𝕜) • rankOne 𝕜 (u i) (v i)
+  orthogonal_right (i j : ℕ) : ⟪v i, v j⟫_𝕜 = 0
+  orthogonal_left (i j : ℕ) : ⟪u i, u j⟫_𝕜 = 0
+
+noncomputable def LinearMap.stdRightSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ V :=
+  Finsupp.embDomain Fin.valEmbedding <|
+    (Finsupp.ofSupportFinite (T.isSymmetric_adjoint_comp_self.eigenvectorBasis rfl))
+    (Set.toFinite _)
+
+noncomputable def LinearMap.stdMinimalLeftSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ U where
+  support := sorry
+  toFun (i : ℕ) := (1 / (T.singularValues i : 𝕜)) • T (T.stdRightSingularVectors i)
+  mem_support_toFun := sorry
+
+-- stdLeftSingularVectors
 
 end inner_product

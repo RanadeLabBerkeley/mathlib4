ee954dd5970693324984cb6ffb769f2e7dc763c3
Niels Voss <code@nielsvoss.com>
2026-05-06 11:04:33 -0700

    Split off orthnormal bases into their own definitions

--- Mathlib/Analysis/InnerProductSpace/SingularVectors.lean
+++ Mathlib/Analysis/InnerProductSpace/SingularVectors.lean
@@ -48,31 +48,38 @@ theorem SingularValueDecomposition.eqOn {T : V →ₗ[𝕜] U} {u : ℕ → U} {
     (hv : Set.Iio (finrank 𝕜 T.range) |>.EqOn v v') : T.SingularValueDecomposition u' v' :=
   hT.right_eqOn hv |>.left_eqOn hu
 
+noncomputable def stdRightSingularOrthonormalBasis (T : V →ₗ[𝕜] U) :
+    OrthonormalBasis (Fin (finrank 𝕜 V)) 𝕜 V :=
+  T.isSymmetric_adjoint_comp_self.eigenvectorBasis rfl
+
 -- TODO: Consider splitting off what's inside the embDomain into a separate function
-noncomputable def stdFullRightSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ V :=
+noncomputable def stdRightFullSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ V :=
   Finsupp.embDomain Fin.valEmbedding <|
-    (Finsupp.ofSupportFinite (T.isSymmetric_adjoint_comp_self.eigenvectorBasis rfl))
+    (Finsupp.ofSupportFinite T.stdRightSingularOrthonormalBasis)
     (Set.toFinite _)
 
-noncomputable def stdCompactRightSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ V :=
-  T.stdFullRightSingularVectors.filter (· < finrank 𝕜 T.range)
+noncomputable def stdRightCompactSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ V :=
+  T.stdRightFullSingularVectors.filter (· < finrank 𝕜 T.range)
 
-noncomputable def stdCompactLeftSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ U where
+noncomputable def stdLeftCompactSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ U where
   support := sorry
-  toFun (i : ℕ) := (1 / (T.singularValues i : 𝕜)) • T (T.stdCompactRightSingularVectors i)
+  toFun (i : ℕ) := (1 / (T.singularValues i : 𝕜)) • T (T.stdRightFullSingularVectors i)
   mem_support_toFun := sorry
 
 -- TODO: Rename
 private theorem helper (T : V →ₗ[𝕜] U)
   : Orthonormal 𝕜 <|
       (Set.Iio ⟨finrank 𝕜 T.range, sorry⟩).restrict
-      (fun i : Fin (finrank 𝕜 U) => T.stdCompactLeftSingularVectors i) := sorry
+      (fun i : Fin (finrank 𝕜 U) => T.stdLeftCompactSingularVectors i) := sorry
+
+noncomputable def stdLeftSingularOrthonormalBasis (T : V →ₗ[𝕜] U) :
+    OrthonormalBasis (Fin (finrank 𝕜 U)) 𝕜 U :=
+  Classical.choose <| T.helper.exists_orthonormalBasis_extension_of_card_eq
+    (Fintype.card_fin _).symm
 
 noncomputable def stdFullLeftSingularVectors (T : V →ₗ[𝕜] U) : ℕ →₀ U :=
-  let x := Classical.choose <| T.helper.exists_orthonormalBasis_extension_of_card_eq
-    (Fintype.card_fin _).symm
   Finsupp.embDomain Fin.valEmbedding <|
-    (Finsupp.ofSupportFinite x)
+    (Finsupp.ofSupportFinite T.stdLeftSingularOrthonormalBasis)
     (Set.toFinite _)
 
 -- Should be able to derive that the stdLeft is singular values from the stdCompact using eqOn
